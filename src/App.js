8fde2ed793410a99b157b606e369a499959e89bd
IlyaLitvin <darnon88@gmail.com>
2021-03-11 14:38:10 +0200

    started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
+import "./App.css";
 
+import Profit from "./components/profit/profit";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hi there</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
