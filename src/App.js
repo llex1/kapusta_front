e99cb83d8ef3b84c30f760cb4cc4bb145cf8df4c
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2021-03-11 17:01:47 +0200

    volodymyr-fon-no-login-multimedia

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Background from './pages/Background/Background.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi there</h1>
+      {/* <h1>hi there</h1> */}
+      <Background />
     </div>
   );
 }
