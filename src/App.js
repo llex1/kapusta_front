2edeed70a24b4565741778c6eaed5926ce47a1ac
ludmiladev <ljuda12@gmail.com>
2021-03-12 00:11:54 +0200

    додала лінк і роутер

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>hi there</h1>
     </div>
   );
