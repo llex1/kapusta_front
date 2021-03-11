8e8cad08064feb30a7af1079850dc78b159b6ae3
Nicholas Trunov <anteycu@gmail.com>
2021-03-11 15:08:20 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Modal from "./modal/modal";
+import IncomeAndCosts from "./components/incomeAndCosts/IncomeAndCosts/IncomeAndCosts"
 
 function App() {
   return (
     <div className="App">
-      <Modal />
+      {/* <Modal /> */}
+      <IncomeAndCosts/>
     </div>
   );
 }
---
+++ src/components/incomeAndCosts/IncomeAndCosts/IncomeAndCosts.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./IncomeAndCosts.module.css";
+
+function IncomeAndCosts() {
+    return (
+        <>
+            <div className={styles.container}>
+                
+
+
+
+        </div>
+        
+        
+        </>
+    )
+}
+export default IncomeAndCosts;
