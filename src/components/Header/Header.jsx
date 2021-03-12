3e93d7598204e7fd4140c33c30f7bfa38390bbc3
ludmiladev <ljuda12@gmail.com>
2021-03-12 10:11:25 +0200

    додала лінк і роутер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,23 @@ import LogoIMG from '../../assets/img/kapusta_main_logo.png';
 import {Link} from 'react-router-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 
+
+
 const Header = () => {
  
   return (
 
 <div>
 <Router>
-
-
 <Link to="/" alt=" " className={styles.Logo_link}>
 <img src={LogoIMG} className={styles.Logo_IMG} alt="Kapusta" />
 </Link>
 
+<UserInfo />
+
 
 </Router>
+
 
 </div>
 
