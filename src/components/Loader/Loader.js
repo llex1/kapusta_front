0e1b16ee94c01aa8d967d49db4250b63e854fe2c
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2021-03-15 21:13:45 +0200

    loader-_-logick

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import style from './Loader.module.css';
+// import Loader from '../../components/Loader/Loader.js';
+//! state Лоадера
+// state = {
+//   loading: true,
+// };
+//!логіка вимкненя Лоадера
+// fetch('http://kapusta.fun/cost')
+// .then(respons=>respons.json())
+// .then(storage=>{
+//   this.setState.loading(false)
+// })
+//! відображення(рендр) Лоадера
+// render({this.setState && <Loader />})
 
 function Loader(props) {
   return (
--- src/pages/Cost/Cost.jsx
+++ src/pages/Cost/Cost.jsx
@@ -3,26 +3,16 @@ import React, { Component, Fragment } from 'react';
 import Header from '../../components/Header';
 import BackgroundAuth from '../../components/BackgroundAuth';
 import CurrentPeriod from '../../components/CurrentPeriod';
-import Loader from '../../components/Loader/Loader.js';
 
 class Cost extends Component {
-  state = {
-    loading: true,
-  };
 
-  //*логіка вимкненя Лоадера
-  // fetch('http://kapusta.fun/cost')
-  // .then(respons=>respons.json())
-  // .then(storage=>{
-  //   this.setState.loading(false)
-  // })
 
   render() {
     return (
       <Fragment>
         <Header />
         <CurrentPeriod />
-        {this.setState && <Loader />}
+
         <BackgroundAuth />
       </Fragment>
     );
