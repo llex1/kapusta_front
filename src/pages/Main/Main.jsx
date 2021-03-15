aac37ee6e9e74dc70c3b00facf90bff06e0cc34e
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2021-03-15 12:32:39 +0200

    start

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,18 +1,19 @@
 import React, { Component, Fragment } from 'react';
 
-import Modal from "../../components/Modal";
-import Header from "../../components/Header";
-import Background from "../../components/Background";
-import IncomeAndCosts from "../../components/IncomeAndCosts"
+import Modal from '../../components/Modal';
+import Header from '../../components/Header';
+import Background from '../../components/Background';
+import IncomeAndCosts from '../../components/IncomeAndCosts';
+import UniversalModal from '../../components/UniversalModal/UniversalModal';
 
 class Main extends Component {
-  
   render() {
     return (
       <Fragment>
         <Header />
         <Modal />
         <Background />
+        <UniversalModal />
       </Fragment>
     );
   }
