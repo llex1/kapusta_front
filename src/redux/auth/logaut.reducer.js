db78a58473126a356ceff38abcb8e8e535eb76e5
ludmiladev <ljuda12@gmail.com>
2021-03-16 13:18:14 +0200

    fix

--- src/redux/auth/logaut.reducer.js
+++ src/redux/auth/logaut.reducer.js
@@ -1,16 +1,16 @@
-import InitState from '../initState.js'
+// import InitState from '../initState.js'
 
-const LogautUserRerucer = (state = InitState.user, action) => {
-    switch (action.type) {
-        case "action-LogautUser":
-            return {...state,
-                       email: action.payload.email,
-                       jwt: action.payload.jwt};
-        default:
-            return {
-                ...state
-            }
-    }
-}
+// const LogautUserRerucer = (state = InitState.user, action) => {
+//     switch (action.type) {
+//         case "action-LogautUser":
+//             return {...state,
+//                        email: action.payload.email,
+//                        jwt: action.payload.jwt};
+//         default:
+//             return {
+//                 ...state
+//             }
+//     }
+// }
 
-export default LogautUserRerucer;
+// export default LogautUserRerucer;
