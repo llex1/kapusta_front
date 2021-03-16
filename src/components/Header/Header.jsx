e26caa051de3eaf0126c6b73859356fbddc93503
ludmiladev <ljuda12@gmail.com>
2021-03-16 17:31:21 +0200

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,9 +15,9 @@ class Header extends Component {
 
    
   name = ()=> {
-   if(this.indexR < 12) {this.indexR.slice(0, this.indexR+1)}
+   if(this.indexR < 8) {this.indexR.slice(0, this.indexR)}
    else {
-    this.indexR.slice(0, 12+1);
+    this.indexR.slice(0, 8+1);
    }
  };
 
