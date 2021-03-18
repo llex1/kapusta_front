c61545b6d93bf8c138428d5e57b59e3e6ff276a0
ludmiladev <ljuda12@gmail.com>
2021-03-18 13:22:31 +0200

    додала сторінку 404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 
 import Main from "./pages/Main";
 import Costs from "./pages/Costs";
+import Page404 from "./pages/Page404";
 
 import "./App.css";
 
@@ -12,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main}></Route>
         <Route exact path="/costs" component={Costs}></Route>
-        {/* <Route component={NotFound}></Route> */}
+        <Route component={Page404}></Route>
       </Switch>
         </Fragment>
   );
