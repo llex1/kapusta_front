7310366348b5fc36a61e98e30df9b16c684f8ad4
ludmiladev <ljuda12@gmail.com>
2021-03-21 07:50:27 +0200

    додала index.js та Page404.jsx із логікою

---
+++ src/pages/Page404/Page404.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Page404.module.css';
+import notFound from './img/notFound.jpg';
+
+
+
+class Page404 extends Component {
+  
+    componentPage404 = () => {
+    
+    };
+  
+    render() {
+      return (
+        <div className="NotFoundWrapper">
+    
+        <br/>
+        <br/>
+        <br/>
+        <h1> Oh Snap!<br/>
+          Error Code : 404 not Found! <br/>
+          <br/>
+    <Link className="link" to='/costs'> To home page</Link>
+        </h1>
+        <img
+        
+              src={notFound}
+              alt="NotFound"
+              aria-label="NotFound"
+              className={styles.img}
+             
+            /> 
+    
+     
+      </div>
+    );
+      }
+    };
+
+
+
+
+  
+  
+  export default Page404;
+  
