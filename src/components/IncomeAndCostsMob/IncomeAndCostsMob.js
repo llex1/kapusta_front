86181c3018b0d1b52959a97107dd093f33831717
Nicholas Trunov <anteycu@gmail.com>
2021-03-23 20:52:19 +0200

    fix some styles

--- src/components/IncomeAndCostsMob/IncomeAndCostsMob.js
+++ src/components/IncomeAndCostsMob/IncomeAndCostsMob.js
@@ -25,34 +25,36 @@ class IncomeAndCostsMob extends Component {
       <div className={styles.container}>
         <BackgroundAuth />
         <div className={styles.positionWrapper}>
-          {this.state.isOpened===true?(<div className={styles.formWrapper}>
+          {this.state.isOpened === true ?
+            (<div className={styles.formWrapper}>
             <IncomeForm onSwitch={this.handleSwitch} title={this.props.title} />
-          </div>):
-          (<div className={styles.showMain}>
+          </div>) : (<>
           <Balance />
           <div className={styles.calendarWrapper}>
             <Calendar />
-            </div>
-            <IncomeTableMob title={this.props.title}/>
-            <div>
-          <NavLink
+              </div>
+              <div className={styles.tableWrapper}>
+                <IncomeTableMob title={this.props.title} />
+                </div>
+            <div className={styles.btnWrapper}>
+                <NavLink
+                   onClick={this.handleSwitch}
             to="/costs"
             className={`${styles.costBtn} ${styles.textBtn}`}
                 activeClassName={styles.active}
-                onClick={this.handleSwitch}
           >
             РАСХОД
           </NavLink>
-          <NavLink
+                <NavLink
+                  onClick={this.handleSwitch}
             to="/profit"
             className={`${styles.incomeBtn} ${styles.textBtn}`}
                 activeClassName={styles.active}
-                onClick={this.handleSwitch}
           >
                 ДОХОД
           </NavLink>
               </div>
-            </div>)}
+              </>)}
         </div>
       </div>
     );
